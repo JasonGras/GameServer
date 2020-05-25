23adc3500bfea08be0c6a6363b30ad4e75ddd881
Grasjason <Grasjason@hotmail.com>
2020-05-25 17:01:18 +0200

    Detect client with Challenge from Cognito = New Password Required Forced the password to a static one for the Test seems Ok. Have to implement the new Scene to handle Redifinition of Password for the client

--- GameServer/SignInClients.cs
+++ GameServer/SignInClients.cs
@@ -15,6 +15,8 @@ namespace GameServer
     {
         public static async Task SignInClientToCognito(string _username, string _password, int _clientid)
         {
+            //var credentials = new BasicAWSCredentials("abc", "xyz");
+
             AmazonCognitoIdentityProviderClient provider =
         new AmazonCognitoIdentityProviderClient(new AnonymousAWSCredentials(), Constants.REGION);
 
@@ -34,48 +36,101 @@ namespace GameServer
             //Console.WriteLine("GetUserAttribute | Pré - isValid : " + user.SessionTokens.IsValid().ToString()); // = Null while SRPAUTH not run
             try
             {
-                Console.WriteLine("Login Lunch");
+                Console.WriteLine("SignInClients.cs | Authentication Requested");
                 authFlowResponse = await user.StartWithSrpAuthAsync(authRequest).ConfigureAwait(false);
-
+                Console.WriteLine("SignInClients.cs | Authentication Success");
                 //authFlowResponse.AuthenticationResult.RefreshToken
                 //Server.clients[_clientid].accessToken = authFlowResponse.AuthenticationResult.AccessToken; // Only Loged In Users have their Access Token Set.
-                if(authFlowResponse.ChallengeName != "{NEW_PASSWORD_REQUIRED}")
-                {                
-                Console.WriteLine("GetUserAttribute | Post - isValid : " + user.SessionTokens.IsValid().ToString());
-                Server.clients[_clientid].myUser = user;
+            
 
-                UserSession uSession = new UserSession(null, null, null);
-                uSession.Access_Token = user.SessionTokens.AccessToken;
-                uSession.Refresh_Token = user.SessionTokens.RefreshToken;
-                uSession.Id_Token = user.SessionTokens.IdToken;
-                ServerSend.SendTokens(_clientid, uSession);
-                Server.clients[_clientid].SendIntoGame(user.Username);
-                }
-                else
-                {
-                    Console.WriteLine("NEW PASSWORD REQUIRED");
-                }
+
+                if (authFlowResponse.ChallengeName == ChallengeNameType.NEW_PASSWORD_REQUIRED)
+                    {
+                        Console.WriteLine("SignInClients.cs | Challenge New Password Required");
+                        // New Password Asked 
+
+                        // New Password Recieved
+                        var nPassword = "Pas0304121988&";
+
+                        // Updating New Password
+                        Console.WriteLine("SignInClients.cs | Updating with New Password");
+                        authFlowResponse = await user.RespondToNewPasswordRequiredAsync(new RespondToNewPasswordRequiredRequest()
+                        {
+                            SessionID = authFlowResponse.SessionID,
+                            NewPassword = nPassword
+                        });
+                        //var accessToken = authFlowResponse.AuthenticationResult.AccessToken;
+                    }
+
+                // Update the Client CognitoUser
+                    Console.WriteLine("SignInClients.cs | Update the Client CognitoUser");
+                    //Console.WriteLine("GetUserAttribute | Post - isValid : " + user.SessionTokens.IsValid().ToString());
+                    Server.clients[_clientid].myUser = user;
+
+                // Initialise Session
+                    Console.WriteLine("SignInClients.cs | Initialize UserSession with Tokens from authentication");
+                    UserSession uSession = new UserSession(null, null, null);
+                    uSession.Access_Token = user.SessionTokens.AccessToken;
+                    uSession.Refresh_Token = user.SessionTokens.RefreshToken;
+                    uSession.Id_Token = user.SessionTokens.IdToken;
+
+                //user.SessionTokens = new CognitoUserSession(user.SessionTokens.IdToken, user.SessionTokens.AccessToken, user.SessionTokens.RefreshToken, DateTime.UtcNow, DateTime.UtcNow.AddDays(1));
+
+                // Send Session
+                    Console.WriteLine("SignInClients.cs | Sending UserSession to Client");
+                    ServerSend.SendTokens(_clientid, uSession);
+
+                // Send User Into Game
+                    Console.WriteLine("SignInClients.cs | Send User Into Game");
+                    Server.clients[_clientid].SendIntoGame(user.Username);
             }
             catch (Exception e)
             {
-                //Win32Exception winEx = e as Win32Exception;
-                
-                //Console.WriteLine(e.GetType());
-                //Console.WriteLine("Login Failed : " + e);
+                Console.WriteLine("SignInClients.cs | My User Authentication Failed");
                 switch (e.GetType().ToString())
                 {
+                    case "Amazon.CognitoIdentityProvider.Model.NotAuthorizedException":
+                        Console.WriteLine("SignInClients.cs | Exception | Unknown Username or password.");
+                        ServerSend.AuthenticationStatus(_clientid, Constants.AUTHENTICATION_KO);
+                        break;
                     case "Amazon.CognitoIdentityProvider.Model.UserNotConfirmedException":
-                        ServerSend.AuthenticationStatus(_clientid, Constants.AUTHENTICATION_USER_CONFIRMED_KO);                        
+                        Console.WriteLine("SignInClients.cs | Exception | Need User Email Confirmation.");
+                        ServerSend.AuthenticationStatus(_clientid, Constants.AUTHENTICATION_USER_CONFIRMED_KO);
                         break;
                     default:
-                        Console.WriteLine(e.GetType().ToString() + " | "+e);
+                        Console.WriteLine("SignInClients.cs | Unknown Exception | "+e.GetType().ToString() + " | " + e);
                         ServerSend.AuthenticationStatus(_clientid, Constants.AUTHENTICATION_KO);
                         break;
                 }
-                
+
                 // Signaler au client que son authentification a Echoué pour X ou Y Raison
-            }  
+            }
+
         }
+       /*public async Task SendUserToChangePasswordSceneAsync(AmazonCognitoIdentityProviderClient provider,string _username, AuthFlowResponse authFlowResponse)
+        {
+            var hash = Util.GetUserPoolSecretHash(_username, Constants.CLIENTAPP_ID, Constants.NeokySecret);
+
+            var challengeResponses = new Dictionary<string, string>
+                    {
+                    { "USERNAME", _username },
+                    { "NEW_PASSWORD", "Pas0304121988&" },
+                    { "SECRET_HASH", hash }
+                    };
+
+            var respondToAuthChallengeRequest = new AdminRespondToAuthChallengeRequest
+            {
+                ChallengeName = ChallengeNameType.NEW_PASSWORD_REQUIRED,
+                ChallengeResponses = challengeResponses,
+                ClientId = Constants.CLIENTAPP_ID,//"app-client-id-here",
+                Session = authFlowResponse.SessionID,
+                UserPoolId = Constants.POOL_ID//"user-pool-id"
+            };
+
+            var challengeResponse = await provider.AdminRespondToAuthChallengeAsync(respondToAuthChallengeRequest).ConfigureAwait(false);
+
+            //challengeResponse will have a populated AuthenticationResult with IdToken, etc.
+        }*/
     }
 }
 /*
